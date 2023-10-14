bf1ab2f9c88c43b0029c0dc8b55171987bfb8f3f
Abhishek <57791251+AbhishekRPanicker@users.noreply.github.com>
2023-10-14 22:49:57 +0530

    Add function to compare player selection and computer selection to determine a winner

--- index.js
+++ index.js
@@ -10,4 +10,34 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice())
+function capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1).toLowerCase();
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = capitalize(playerSelection);
+
+
+    if (playerSelection === computerSelection) {
+        return `It's a Draw! ${playerSelection} draws ${computerSelection}`;
+    } else if (
+        playerSelection === "Rock" && computerSelection === "Paper"
+        || playerSelection === "Paper" && computerSelection === "Scissors"
+        || playerSelection === "Scissors" && computerSelection === "Rock"
+    ) {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    } else if (
+        playerSelection === "Rock" && computerSelection === "Scissors"
+        || playerSelection === "Paper" && computerSelection === "Rock"
+        || playerSelection === "Scissors" && computerSelection === "Paper"
+    ) {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return "Invalid Choice! Try again.";
+    }
+}
+
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
